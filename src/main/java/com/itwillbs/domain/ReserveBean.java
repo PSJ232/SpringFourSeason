73006dea3ee62dd263903d491f1c4bbc3eab1334
gumgu <bienlee@naver.com>
2021-08-29 16:20:03 +0900

    ReserveBean

---
+++ src/main/java/com/itwillbs/domain/ReserveBean.java
@@ -0,0 +1,85 @@
+package com.itwillbs.domain;
+
+public class ReserveBean {
+    private int r_id;
+    private String m_id;
+    private int f_id;
+    private int fd_id;
+    private int r_num;
+    private String r_payment;
+    private String r_date;
+    private int r_confirm;
+    private int r_amount;
+
+    public int getR_id() {
+        return r_id;
+    }
+
+    public void setR_id(int r_id) {
+        this.r_id = r_id;
+    }
+
+    public String getM_id() {
+        return m_id;
+    }
+
+    public void setM_id(String m_id) {
+        this.m_id = m_id;
+    }
+
+    public int getF_id() {
+        return f_id;
+    }
+
+    public void setF_id(int f_id) {
+        this.f_id = f_id;
+    }
+
+    public int getFd_id() {
+        return fd_id;
+    }
+
+    public void setFd_id(int fd_id) {
+        this.fd_id = fd_id;
+    }
+
+    public int getR_num() {
+        return r_num;
+    }
+
+    public void setR_num(int r_num) {
+        this.r_num = r_num;
+    }
+
+    public String getR_payment() {
+        return r_payment;
+    }
+
+    public void setR_payment(String r_payment) {
+        this.r_payment = r_payment;
+    }
+
+    public String getR_date() {
+        return r_date;
+    }
+
+    public void setR_date(String r_date) {
+        this.r_date = r_date;
+    }
+
+    public int getR_confirm() {
+        return r_confirm;
+    }
+
+    public void setR_confirm(int r_confirm) {
+        this.r_confirm = r_confirm;
+    }
+
+    public int getR_amount() {
+        return r_amount;
+    }
+
+    public void setR_amount(int r_amount) {
+        this.r_amount = r_amount;
+    }
+}
